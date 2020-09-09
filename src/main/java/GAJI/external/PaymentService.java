f79421bb520e7f906d3c22c778a23dda7debd905
Muryong-Jeon <muryongjeon@gmail.com>
2020-09-09 18:04:01 +0900

    aa

--- src/main/java/GAJI/external/PaymentService.java
+++ src/main/java/GAJI/external/PaymentService.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import java.util.Date;
 
 //@FeignClient(name="Payment", url="http://localhost:8084")
